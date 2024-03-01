db2ea422c4269c943ab7ea1bb17fe69ba5848e63
siemah <hmi.dayenio@gmail.com>
2024-03-01 16:28:24 +0100

    ref:cookie

--- src/app/signin/action.ts
+++ src/app/signin/action.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { cookies } from "next/headers";
+import cookie from "cookie";
+
 export async function signin(fd: FormData) {
   try {
     const url = `https://soukesmar.com/wp-json/zz-mobile-app/v1/auth/signin`;
@@ -15,9 +18,39 @@ export async function signin(fd: FormData) {
       },
       body: JSON.stringify(credentials),
     });
+    console.log("----------------------------");
+    const rawCookie = request.headers.get("set-cookie");
+    const expires = rawCookie?.match(/expires=[^;]+;?/g);
+    const rawCookieWithouExpires = rawCookie?.replace(/expires=[^;]+;?/g, "");
+    console.log(
+      "---------------=======-------------",
+      expires,
+      // rawCookieWithouExpires,
+      // cookie.parse(`${rawCookieWithouExpires}`),
+    );
+    rawCookieWithouExpires?.split(", ").map((cookieItem, index) => {
+      const parsedCookie = cookie.parse(cookieItem);
+      const [[name, value]] = Object.entries(parsedCookie);
+      // @ts-ignore
+      const res = cookies().set(name, value, {
+        domain: parsedCookie.domain,
+        maxAge: parsedCookie?.["Max-Age"],
+        path: parsedCookie.path,
+        expires: expires?.[index],
+        secure: true,
+      });
+      console.log(`cookie ${name} added`);
+    });
+    // request.headers
+    //   .getSetCookie()
+    //   .map((current) => console.log(`cookie ->`, cookie.parse(current)));
+    // const _cookies = cookies();
+    // _cookies.set("_uid", `${Date.now()}`);
     const response = await request.json();
-    console.log(`response`, response);
+    console.log(`response`, response.code);
+    return response.code;
   } catch (error) {
     console.log("[Error]", error);
+    return error;
   }
 }
